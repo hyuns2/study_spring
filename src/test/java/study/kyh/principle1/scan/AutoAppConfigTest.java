ce66725947e67698d6116dc19c89e4fd780f053a
hyuns2 <hyunsoi0103@gmail.com>
2023-10-09 14:37:56 +0900

    Test: add to AutoAppConfigTest

---
+++ src/test/java/study/kyh/principle1/scan/AutoAppConfigTest.java
@@ -0,0 +1,20 @@
+package study.kyh.principle1.scan;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import study.kyh.principle1.AutoAppConfig;
+import study.kyh.principle1.member.MemberService;
+
+public class AutoAppConfigTest {
+
+    @Test
+    void basicScan() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+
+        MemberService memberService = ac.getBean(MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+
+}
