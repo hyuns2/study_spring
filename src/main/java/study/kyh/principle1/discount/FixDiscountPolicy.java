fb672d55d11c3c7c2705e4b516da9bc76c9080e9
hyuns2 <hyunsoi0103@gmail.com>
2023-10-12 15:07:03 +0900

    Test: add to AllBeanTest

--- src/main/java/study/kyh/principle1/discount/FixDiscountPolicy.java
+++ src/main/java/study/kyh/principle1/discount/FixDiscountPolicy.java
@@ -1,8 +1,12 @@
 package study.kyh.principle1.discount;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
 import study.kyh.principle1.member.Grade;
 import study.kyh.principle1.member.Member;
 
+@Component
+@Primary
 public class FixDiscountPolicy implements DiscountPolicy{
 
     private int discountFixAmount = 1000;
