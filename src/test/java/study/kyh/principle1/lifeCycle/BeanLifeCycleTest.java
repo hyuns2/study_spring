81bfb7700db67f9fdcf12a1f8e37be606167f71e
hyuns2 <hyunsoi0103@gmail.com>
2023-10-12 15:26:18 +0900

    Test: add to lifeCycle

---
+++ src/test/java/study/kyh/principle1/lifeCycle/BeanLifeCycleTest.java
@@ -0,0 +1,30 @@
+package study.kyh.principle1.lifeCycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+
+        NetworkClient networkClient = ac.getBean(NetworkClient.class);
+
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+        @Bean // (initMethod = "init", destroyMethod = "close")
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+
+}
---
+++ src/test/java/study/kyh/principle1/lifeCycle/NetworkClient.java
@@ -0,0 +1,45 @@
+package study.kyh.principle1.lifeCycle;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void connect() {
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call: " + url + ", message: " + message);
+    }
+
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disconnect();
+    }
+
+}
