eade2282fc649d1424c09f9e373006917ba23da2
hyuns2 <hyunsoi0103@gmail.com>
2023-10-30 21:34:59 +0900

    Feat: add to ReqeustParamServlet

---
+++ src/main/java/study/kyh/mvc/basic/RequestParamServlet.java
@@ -0,0 +1,24 @@
+package study.kyh.mvc.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("[전체 파라미터 조회] - start");
+        req.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> System.out.println(paramName + "=" + req.getParameter(paramName)));
+        System.out.println("[전체 파라미터 조회] - end");
+        System.out.println();
+
+    }
+}
