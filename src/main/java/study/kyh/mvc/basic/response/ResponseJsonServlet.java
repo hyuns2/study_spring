b357bf269655284d441b8f2a35012779c7991d68
hyuns2 <hyunsoi0103@gmail.com>
2023-10-30 21:56:35 +0900

    Feat: add to ResponseJsonServlet

---
+++ src/main/java/study/kyh/mvc/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,31 @@
+package study.kyh.mvc.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import study.kyh.mvc.basic.HelloData;
+
+import java.io.IOException;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setHeader("content-type", "application/json");
+        resp.setCharacterEncoding("utf-8");
+
+        HelloData data = new HelloData();
+        data.setUsername("kim");
+        data.setAge(20);
+
+        String result = objectMapper.writeValueAsString(data);
+
+        resp.getWriter().write(result);
+    }
+}
