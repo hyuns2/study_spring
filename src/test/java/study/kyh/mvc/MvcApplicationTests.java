964802d2f3d9364f43bbd7919171ec87b57c6b15
hyuns2 <hyunsoi0103@gmail.com>
2023-10-30 20:57:35 +0900

    Chore: begin

---
+++ src/test/java/study/kyh/mvc/MvcApplicationTests.java
@@ -0,0 +1,13 @@
+package study.kyh.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
