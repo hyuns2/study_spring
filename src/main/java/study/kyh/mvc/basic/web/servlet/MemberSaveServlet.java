df7f2f113a09c74c53a98d93a1b0d60ef8557061
hyuns2 <hyunsoi0103@gmail.com>
2023-11-03 19:33:36 +0900

    Feat: add to web.servlet

---
+++ src/main/java/study/kyh/mvc/basic/web/servlet/MemberSaveServlet.java
@@ -0,0 +1,48 @@
+package study.kyh.mvc.basic.web.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import study.kyh.mvc.basic.domain.member.Member;
+import study.kyh.mvc.basic.domain.member.MemberRepository;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "memberSaveServlet", urlPatterns = "/servlet/members/save")
+public class MemberSaveServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("MemberSaveServlet.service");
+        String userName = req.getParameter("username");
+        int age = Integer.parseInt(req.getParameter("age"));
+
+        Member member = new Member(userName, age);
+        System.out.println("member = " + member);
+        memberRepository.save(member);
+
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
+
+        PrintWriter w = resp.getWriter();
+        w.write("<html>\n" +
+                "<head>\n" +
+                " <meta charset=\"UTF-8\">\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "성공\n" +
+                "<ul>\n" +
+                " <li>id="+member.getId()+"</li>\n" +
+                " <li>username="+member.getUserName()+"</li>\n" +
+                " <li>age="+member.getAge()+"</li>\n" +
+                "</ul>\n" +
+                "<a href=\"/index.html\">메인</a>\n" +
+                "</body>\n" +
+                "</html>");
+    }
+}
