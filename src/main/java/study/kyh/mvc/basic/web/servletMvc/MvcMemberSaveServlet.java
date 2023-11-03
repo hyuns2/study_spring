1b7d963664f1a9a19e9930182504050a58822d38
hyuns2 <hyunsoi0103@gmail.com>
2023-11-03 20:21:19 +0900

    Feat: add to MvcMemberSaveServlet

---
+++ src/main/java/study/kyh/mvc/basic/web/servletMvc/MvcMemberSaveServlet.java
@@ -0,0 +1,34 @@
+package study.kyh.mvc.basic.web.servletMvc;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import study.kyh.mvc.basic.domain.member.Member;
+import study.kyh.mvc.basic.domain.member.MemberRepository;
+
+import java.io.IOException;
+
+@WebServlet(name = "mvcMemberSaveServlet", urlPatterns = "/servlet-mvc/members/save")
+public class MvcMemberSaveServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userName = req.getParameter("username");
+        int age = Integer.parseInt(req.getParameter("age"));
+
+        Member member = new Member(userName, age);
+        System.out.println("member = " + member);
+        memberRepository.save(member);
+
+        req.setAttribute("member", member);
+
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
+        dispatcher.forward(req, resp);
+    }
+}
