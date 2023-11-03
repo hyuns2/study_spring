3d0754b0259a54672cba060a3ee2930d97ac771f
hyuns2 <hyunsoi0103@gmail.com>
2023-11-03 20:29:20 +0900

    Feat: add to MvcMemberListServlet

---
+++ src/main/java/study/kyh/mvc/basic/web/servletMvc/MvcMemberListServlet.java
@@ -0,0 +1,31 @@
+package study.kyh.mvc.basic.web.servletMvc;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import study.kyh.mvc.basic.domain.member.Member;
+import study.kyh.mvc.basic.domain.member.MemberRepository;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "mvcMemberListServlet", urlPatterns = "/servlet-mvc/members")
+public class MvcMemberListServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("MvcMemberListServlet.service");
+        List<Member> members = memberRepository.findAll();
+
+        req.setAttribute("members", members);
+
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
+        dispatcher.forward(req, resp);
+    }
+}
