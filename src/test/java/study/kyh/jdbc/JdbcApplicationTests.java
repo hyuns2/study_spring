1c062aa5361850907e976984b5f6e85735a5a1d2
hyuns2 <hyunsoi0103@gmail.com>
2023-11-05 20:22:05 +0900

    Feat: add to MemberRepositoryV0

---
+++ src/test/java/study/kyh/jdbc/JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package study.kyh.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
