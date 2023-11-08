1bcc973f1ca73da3f4f68284f0515d7146c90b13
hyuns2 <hyunsoi0103@gmail.com>
2023-11-08 13:56:43 +0900

    Feat: add to springMvc/old

---
+++ src/main/java/study/kyh/mvc/basic/web/springMvc/old/MyHttpRequestHandler.java
@@ -0,0 +1,19 @@
+package study.kyh.mvc.basic.web.springMvc.old;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.HttpRequestHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+
+@Component("/springmvc/request-handler")
+public class MyHttpRequestHandler implements HttpRequestHandler {
+
+    @Override
+    public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MyHttpRequestHandler.handleRequest");
+    }
+}
