c10559f946744df97107be942fdf45c1b1513643
hyuns2 <hyunsoi0103@gmail.com>
2023-11-09 21:47:43 +0900

    Fix: add to constructor in domain Item

--- src/main/java/study/kyh/mvc2/domain/item/Item.java
+++ src/main/java/study/kyh/mvc2/domain/item/Item.java
@@ -1,11 +1,9 @@
 package study.kyh.mvc2.domain.item;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Data
 public class Item {
 
@@ -13,5 +11,11 @@ public class Item {
     private String itemName;
     private Integer price;
     private Integer quantity;
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
 
 }
