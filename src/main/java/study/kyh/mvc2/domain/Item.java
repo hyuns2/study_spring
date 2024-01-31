5098363045baaae91436497969f9efc34dfd4827
hyuns2 <hyunsoi0103@gmail.com>
2024-02-01 02:04:27 +0900

    Test: add to ItemRepositoryTest

--- src/main/java/study/kyh/mvc2/domain/Item.java
+++ src/main/java/study/kyh/mvc2/domain/Item.java
@@ -6,12 +6,17 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class Item {
 
     private Long id;
     private String itemName;
     private Integer price;
     private Integer quantity;
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
 
 }
