a83992198c5bf817b9f8b00e6cf6a1171e621e04
hyuns2 <hyunsoi0103@gmail.com>
2024-02-01 01:47:52 +0900

    Feat: add to domain

---
+++ src/main/java/study/kyh/mvc2/domain/Item.java
@@ -0,0 +1,17 @@
+package study.kyh.mvc2.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+}
