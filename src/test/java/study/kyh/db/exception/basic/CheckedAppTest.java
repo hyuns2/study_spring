839353c27836aa0c09080789444a425aff3c64e8
hyuns2 <hyunsoi0103@gmail.com>
2024-02-04 20:50:34 +0900

    Test: add to exception app test

---
+++ src/test/java/study/kyh/db/exception/basic/CheckedAppTest.java
@@ -0,0 +1,49 @@
+package study.kyh.db.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.net.ConnectException;
+import java.sql.SQLException;
+
+@Slf4j
+public class CheckedAppTest {
+
+    @Test
+    void checked() {
+        Controller controller = new Controller();
+        Assertions.assertThatThrownBy(() -> controller.request()).isInstanceOf(Exception.class);
+    }
+
+    static class Controller {
+        Service service = new Service();
+
+        public void request() throws ConnectException, SQLException {
+            service.logic();
+        }
+    }
+
+    static class Service {
+        Repository repository = new Repository();
+        NetworkClient networkClient = new NetworkClient();
+
+        public void logic() throws ConnectException, SQLException {
+            networkClient.call();
+            repository.call();
+        }
+    }
+
+    static class NetworkClient {
+        public void call() throws ConnectException {
+            throw new ConnectException("연결 실패");
+        }
+    }
+
+    static class Repository {
+        public void call() throws SQLException {
+            throw new SQLException("ex");
+        }
+    }
+
+}
