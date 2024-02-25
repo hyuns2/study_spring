fe20c686570bbb9829f38e531231213b0ab09769
hyuns2 <hyunsoi0103@gmail.com>
2024-02-25 17:48:58 +0900

    Feat: add to HelloTraceV2

--- src/main/java/study/kyh/principle2/trace/helloTrace/HelloTraceV2.java
+++ src/main/java/study/kyh/principle2/trace/helloTrace/HelloTraceV2.java
@@ -7,7 +7,7 @@ import study.kyh.principle2.trace.TraceStatus;
 
 @Slf4j
 @Component
-public class HelloTraceV1 {
+public class HelloTraceV2 {
 
     private static final String START_PREFIX = "-->";
     private static final String COMPLETE_PREFIX = "<--";
@@ -18,6 +18,14 @@ public class HelloTraceV1 {
         Long startTimeMs = System.currentTimeMillis();
         log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX, traceId.getLevel()), message);
         return new TraceStatus(traceId, startTimeMs, message);
+    }
+
+    public TraceStatus beginSync(TraceId beforeTraceId, String message) {
+        TraceId nextId = beforeTraceId.createNextId();
+        Long startTimeMs = System.currentTimeMillis();
+        log.info("[{}] {}{}", nextId.getId(), addSpace(START_PREFIX, nextId.getLevel()), message);
+        return new TraceStatus(nextId, startTimeMs, message);
+
     }
 
     private static String addSpace(String prefix, int level) {
