9d7655098cf4b2c6adce7a5eb8952fac2aca9349
hyuns2 <hyunsoi0103@gmail.com>
2024-02-25 18:47:10 +0900

    Test: add to FieldService and its test

---
+++ src/test/java/study/kyh/principle2/trace/threadLocal/FieldServiceTest.java
@@ -0,0 +1,45 @@
+package study.kyh.principle2.trace.threadLocal;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import study.kyh.principle2.trace.threadLocal.code.FieldService;
+
+@Slf4j
+public class FieldServiceTest {
+
+    private FieldService fieldService = new FieldService();
+
+    @Test
+    void field() {
+        log.info("main start");
+
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("threadA");
+        Thread threadB = new Thread(userB);
+        threadB.setName("threadB");
+
+        threadA.start();
+//        sleep(2000);
+        sleep(100);
+        threadB.start();
+
+        sleep(3000);
+        log.info("main exit");
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
