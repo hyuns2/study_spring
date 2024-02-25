28b64bd2ec7db2e71f357abf431a34e321368025
hyuns2 <hyunsoi0103@gmail.com>
2024-02-25 18:15:51 +0900

    Test: add to FieldLogTrace

---
+++ src/test/java/study/kyh/principle2/trace/logTrace/FieldLogTraceTest.java
@@ -0,0 +1,26 @@
+package study.kyh.principle2.trace.logTrace;
+
+import org.junit.jupiter.api.Test;
+import study.kyh.principle2.trace.TraceStatus;
+import study.kyh.principle2.trace.helloTrace.HelloTraceV2;
+
+public class FieldLogTraceTest {
+
+    FieldLogTrace trace = new FieldLogTrace();
+
+    @Test
+    void begin_end_level2() {
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.begin("hello2");
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+    @Test
+    void begin_exception_level2() {
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.begin("hello2");
+        trace.exception(status2, new IllegalStateException());
+        trace.exception(status1, new IllegalStateException());
+    }
+}
