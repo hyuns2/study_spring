c76e1ac2eb7095838c3e457dc8b383da2f6d1a06
hyuns2 <hyunsoi0103@gmail.com>
2024-03-07 13:17:46 +0900

    Feat: add AbstractTemplate

---
+++ src/main/java/study/kyh/principle2/trace/AbstractTemplate.java
@@ -0,0 +1,28 @@
+package study.kyh.principle2.trace;
+
+import study.kyh.principle2.trace.logTrace.LogTrace;
+
+public abstract class AbstractTemplate<T> {
+
+    private final LogTrace trace;
+
+    public AbstractTemplate(LogTrace trace) {
+        this.trace = trace;
+    }
+
+    public T execute(String message) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin(message);
+            T result = call();
+            trace.end(status);
+            return result;
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+    protected abstract T call();
+
+}
