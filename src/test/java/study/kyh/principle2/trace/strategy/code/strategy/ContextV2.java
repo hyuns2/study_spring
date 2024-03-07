0010f87424790b377415375e0986b8beed9adce3
hyuns2 <hyunsoi0103@gmail.com>
2024-03-07 14:11:40 +0900

    Test: add ContextV2

---
+++ src/test/java/study/kyh/principle2/trace/strategy/code/strategy/ContextV2.java
@@ -0,0 +1,18 @@
+package study.kyh.principle2.trace.strategy.code.strategy;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ContextV2 {
+
+    public void execute(Strategy strategy) {
+        long startTime = System.currentTimeMillis();
+
+        strategy.call();
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+}
