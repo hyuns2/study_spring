5997653a9852a89608eddf387e7501f8e90c402e
hyuns2 <hyunsoi0103@gmail.com>
2024-03-07 10:10:21 +0900

    Test: add TemplateMethodV2

--- src/test/java/study/kyh/principle2/trace/template/TemplateMethodTest.java
+++ src/test/java/study/kyh/principle2/trace/template/TemplateMethodTest.java
@@ -2,6 +2,9 @@ package study.kyh.principle2.trace.template;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import study.kyh.principle2.trace.template.code.AbstractTemplate;
+import study.kyh.principle2.trace.template.code.SubClassLogic1;
+import study.kyh.principle2.trace.template.code.SubClassLogic2;
 
 @Slf4j
 public class TemplateMethodTest {
@@ -10,6 +13,36 @@ public class TemplateMethodTest {
     void templateMethodV0() {
         logic1();
         logic2();
+    }
+
+    @Test
+    void templateMethodV1() {
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.execute();
+
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
+    }
+
+    @Test
+    void templateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        log.info("클래스 이름1={}", template1.getClass());
+        template1.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+        log.info("클래스 이름2={}", template2.getClass());
+        template2.execute();
     }
 
     private void logic1() {
