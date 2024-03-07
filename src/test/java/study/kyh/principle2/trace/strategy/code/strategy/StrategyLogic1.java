48d48e89413084bb64f35d563fc6bf6f822e44f8
hyuns2 <hyunsoi0103@gmail.com>
2024-03-07 13:48:24 +0900

    Test: add StrategyLogics

---
+++ src/test/java/study/kyh/principle2/trace/strategy/code/strategy/StrategyLogic1.java
@@ -0,0 +1,12 @@
+package study.kyh.principle2.trace.strategy.code.strategy;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class StrategyLogic1 implements Strategy {
+
+    @Override
+    public void call() {
+        log.info("비즈니스 로직1 실행");
+    }
+}
