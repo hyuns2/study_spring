852613f83235c9ef6072a2804f2c7eb1b7ac2511
hyuns2 <hyunsoi0103@gmail.com>
2024-03-07 13:58:49 +0900

    Test: add to ContextV1

--- src/test/java/study/kyh/principle2/trace/strategy/ContextV1Test.java
+++ src/test/java/study/kyh/principle2/trace/strategy/ContextV1Test.java
@@ -2,6 +2,10 @@ package study.kyh.principle2.trace.strategy;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import study.kyh.principle2.trace.strategy.code.strategy.ContextV1;
+import study.kyh.principle2.trace.strategy.code.strategy.Strategy;
+import study.kyh.principle2.trace.strategy.code.strategy.StrategyLogic1;
+import study.kyh.principle2.trace.strategy.code.strategy.StrategyLogic2;
 
 @Slf4j
 public class ContextV1Test {
@@ -10,6 +14,17 @@ public class ContextV1Test {
     void strategyV0() {
         logic1();
         logic2();
+    }
+
+    @Test
+    void strategyV1() {
+        Strategy strategyLogic1 = new StrategyLogic1();
+        ContextV1 context1 = new ContextV1(strategyLogic1);
+        context1.execute();
+
+        Strategy strategyLogic2 = new StrategyLogic2();
+        ContextV1 context2 = new ContextV1(strategyLogic2);
+        context2.execute();
     }
 
     private void logic1() {
