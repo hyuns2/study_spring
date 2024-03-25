b24cd8e7b3ffa20884efaa535924b89975e57764
hyuns2 <hyunsoi0103@gmail.com>
2024-03-25 20:47:25 +0900

    Test: add CacheProxy

---
+++ src/test/java/study/kyh/principle3/pure_proxy/proxy/code/CacheProxy.java
@@ -0,0 +1,22 @@
+package study.kyh.principle3.pure_proxy.proxy.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CacheProxy implements Subject {
+    private Subject target;
+    private String cacheValue;
+
+    public CacheProxy(Subject target) {
+        this.target = target;
+    }
+
+    @Override
+    public String operation() {
+        log.info("프록시 호출");
+        if (cacheValue == null) {
+            cacheValue = target.operation();
+        }
+        return cacheValue;
+    }
+}
