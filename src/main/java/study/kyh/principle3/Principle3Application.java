fbbc5fe00d22c1c194ab1641c1b695f9dd4ab897
hyuns2 <hyunsoi0103@gmail.com>
2024-03-25 18:05:27 +0900

    Feat: add v3

--- src/main/java/study/kyh/principle3/Principle3Application.java
+++ src/main/java/study/kyh/principle3/Principle3Application.java
@@ -7,7 +7,7 @@ import study.kyh.principle3.config.AppV1Config;
 import study.kyh.principle3.config.AppV2Config;
 
 @Import({AppV1Config.class, AppV2Config.class})
-@SpringBootApplication(scanBasePackages = "study.kyh.app.v3")
+@SpringBootApplication(scanBasePackages = "study.kyh.principle3.app.v3")
 public class Principle3Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/study/kyh/principle3/app/v3/OrderServiceV3.java
@@ -0,0 +1,16 @@
+package study.kyh.principle3.app.v3;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceV3 {
+    private final OrderRepositoryV3 orderRepository;
+
+    public OrderServiceV3(OrderRepositoryV3 orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public void orderItem(String itemId) {
+        orderRepository.save(itemId);
+    }
+}
