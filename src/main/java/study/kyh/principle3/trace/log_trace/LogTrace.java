bcc5d7f0d54b0a1d3c0c499e65ae8ccc79967e3c
hyuns2 <hyunsoi0103@gmail.com>
2024-03-26 17:18:01 +0900

    Feat: add trace

---
+++ src/main/java/study/kyh/principle3/trace/log_trace/LogTrace.java
@@ -0,0 +1,11 @@
+package study.kyh.principle3.trace.log_trace;
+
+import study.kyh.principle2.trace.TraceStatus;
+
+public interface LogTrace {
+
+    TraceStatus begin(String message);
+    void end(TraceStatus status);
+    void exception(TraceStatus status, Exception e);
+
+}
