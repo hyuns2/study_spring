70310732f6304acb9f13d7fd41d9abd6f0d3984c
hyuns2 <hyunsoi0103@gmail.com>
2024-03-26 18:12:56 +0900

    Test: add concrete_proxy.code

---
+++ src/test/java/study/kyh/principle3/pure_proxy/concrete_proxy/code/ConcreteClient.java
@@ -0,0 +1,13 @@
+package study.kyh.principle3.pure_proxy.concrete_proxy.code;
+
+public class ConcreteClient {
+    private ConcreteLogic concreteLogic;
+
+    public ConcreteClient(ConcreteLogic concreteLogic) {
+        this.concreteLogic = concreteLogic;
+    }
+
+    public void execute() {
+        concreteLogic.operation();
+    }
+}
