668ea8d982966664cea4fa5bac849efe0944eb53
hyuns2 <hyunsoi0103@gmail.com>
2024-03-26 18:38:27 +0900

    Fix: update ConcreteProxyConfig

--- src/main/java/study/kyh/principle3/Principle3Application.java
+++ src/main/java/study/kyh/principle3/Principle3Application.java
@@ -6,12 +6,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import study.kyh.principle3.config.AppV1Config;
 import study.kyh.principle3.config.AppV2Config;
+import study.kyh.principle3.config.v1_proxy.ConcreteProxyConfig;
 import study.kyh.principle3.config.v1_proxy.InterfaceProxyConfig;
 import study.kyh.principle3.trace.log_trace.LogTrace;
 import study.kyh.principle3.trace.log_trace.ThreadLocalLogTrace;
 
 // @Import({AppV1Config.class, AppV2Config.class})
-@Import(InterfaceProxyConfig.class)
+// @Import(InterfaceProxyConfig.class)
+@Import(ConcreteProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "study.kyh.principle3.app.v3")
 public class Principle3Application {
 
--- src/main/java/study/kyh/principle3/config/v1_proxy/ConcreteProxyConfig.java
+++ src/main/java/study/kyh/principle3/config/v1_proxy/ConcreteProxyConfig.java
@@ -3,9 +3,9 @@ package study.kyh.principle3.config.v1_proxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import study.kyh.principle3.app.v2.*;
-import study.kyh.principle3.config.v1_proxy.interface_proxy.OrderControllerInterfaceProxy;
-import study.kyh.principle3.config.v1_proxy.interface_proxy.OrderRepositoryInterfaceProxy;
-import study.kyh.principle3.config.v1_proxy.interface_proxy.OrderServiceInterfaceProxy;
+import study.kyh.principle3.config.v1_proxy.concrete_proxy.OrderControllerConcreteProxy;
+import study.kyh.principle3.config.v1_proxy.concrete_proxy.OrderRepositoryConcreteProxy;
+import study.kyh.principle3.config.v1_proxy.concrete_proxy.OrderServiceConcreteProxy;
 import study.kyh.principle3.trace.log_trace.LogTrace;
 
 @Configuration
@@ -13,18 +13,18 @@ public class ConcreteProxyConfig {
     @Bean
     public OrderControllerV2 orderController(LogTrace logTrace) {
         OrderControllerV2 controllerImpl = new OrderControllerV2(orderService(logTrace));
-        return new OrderControllerInterfaceProxy(controllerImpl, logTrace);
+        return new OrderControllerConcreteProxy(controllerImpl, logTrace);
     }
 
     @Bean
     public OrderServiceV2 orderService(LogTrace logTrace) {
         OrderServiceV2 serviceImpl = new OrderServiceV2(orderRepository(logTrace));
-        return new OrderServiceInterfaceProxy(serviceImpl, logTrace);
+        return new OrderServiceConcreteProxy(serviceImpl, logTrace);
     }
 
     @Bean
     public OrderRepositoryV2 orderRepository(LogTrace logTrace) {
         OrderRepositoryV2 repositoryImpl = new OrderRepositoryV2();
-        return new OrderRepositoryInterfaceProxy(repositoryImpl, logTrace);
+        return new OrderRepositoryConcreteProxy(repositoryImpl, logTrace);
     }
 }
