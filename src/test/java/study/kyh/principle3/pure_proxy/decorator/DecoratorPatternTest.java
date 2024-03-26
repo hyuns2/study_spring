43d06843c777d694e915cf9a4f08a0b9c46973fd
hyuns2 <hyunsoi0103@gmail.com>
2024-03-26 16:56:54 +0900

    Test: add decorator1

--- src/test/java/study/kyh/principle3/pure_proxy/decorator/DecoratorPatternTest.java
+++ src/test/java/study/kyh/principle3/pure_proxy/decorator/DecoratorPatternTest.java
@@ -3,6 +3,7 @@ package study.kyh.principle3.pure_proxy.decorator;
 import org.junit.jupiter.api.Test;
 import study.kyh.principle3.pure_proxy.decorator.code.Component;
 import study.kyh.principle3.pure_proxy.decorator.code.DecoratorPatternClient;
+import study.kyh.principle3.pure_proxy.decorator.code.MessageDecorator;
 import study.kyh.principle3.pure_proxy.decorator.code.RealComponent;
 
 public class DecoratorPatternTest {
@@ -10,6 +11,14 @@ public class DecoratorPatternTest {
     void noDecorator() {
         Component realComponent = new RealComponent();
         DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+        client.execute();
+    }
+
+    @Test
+    void decorator1() {
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
         client.execute();
     }
 }
