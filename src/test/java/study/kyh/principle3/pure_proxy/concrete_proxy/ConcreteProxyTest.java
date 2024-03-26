f25776f4b5fc1d9e2a01bead61fc9787c4d748ba
hyuns2 <hyunsoi0103@gmail.com>
2024-03-26 18:18:59 +0900

    Test: add addProxy

--- src/test/java/study/kyh/principle3/pure_proxy/concrete_proxy/ConcreteProxyTest.java
+++ src/test/java/study/kyh/principle3/pure_proxy/concrete_proxy/ConcreteProxyTest.java
@@ -3,12 +3,21 @@ package study.kyh.principle3.pure_proxy.concrete_proxy;
 import org.junit.jupiter.api.Test;
 import study.kyh.principle3.pure_proxy.concrete_proxy.code.ConcreteClient;
 import study.kyh.principle3.pure_proxy.concrete_proxy.code.ConcreteLogic;
+import study.kyh.principle3.pure_proxy.concrete_proxy.code.TimeProxy;
 
 public class ConcreteProxyTest {
     @Test
     void noProxy() {
         ConcreteLogic concreteLogic = new ConcreteLogic();
         ConcreteClient concreteClient = new ConcreteClient(concreteLogic);
+        concreteClient.execute();
+    }
+
+    @Test
+    void addProxy() {
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        TimeProxy timeProxy = new TimeProxy(concreteLogic);
+        ConcreteClient concreteClient = new ConcreteClient(timeProxy);
         concreteClient.execute();
     }
 }
