a04fa35c31312e8c748c6fb6991736c684229cbd
hyuns2 <hyunsoi0103@gmail.com>
2024-03-26 16:50:14 +0900

    Test: add MessageDecorator

---
+++ src/test/java/study/kyh/principle3/pure_proxy/decorator/code/MessageDecorator.java
@@ -0,0 +1,23 @@
+package study.kyh.principle3.pure_proxy.decorator.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MessageDecorator implements Component {
+    private Component component;
+
+    public MessageDecorator(Component component) {
+        this.component = component;
+    }
+
+    @Override
+    public String operation() {
+        log.info("MessageDecorator 실행");
+
+        String result = component.operation();
+        String decoResult = "*****" + result + "*****";
+        log.info("MessageDecorator 꾸미기 전={}, 적용 후={}", result, decoResult);
+
+        return decoResult;
+    }
+}
