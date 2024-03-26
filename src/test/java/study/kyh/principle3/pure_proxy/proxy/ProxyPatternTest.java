1c8d7d8a9e324e6da47414a8143b1f97d1a87b1e
hyuns2 <hyunsoi0103@gmail.com>
2024-03-26 16:37:23 +0900

    Test: add cacheProxyTest

--- src/test/java/study/kyh/principle3/pure_proxy/proxy/ProxyPatternTest.java
+++ src/test/java/study/kyh/principle3/pure_proxy/proxy/ProxyPatternTest.java
@@ -1,14 +1,26 @@
 package study.kyh.principle3.pure_proxy.proxy;
 
 import org.junit.jupiter.api.Test;
+import study.kyh.principle3.pure_proxy.proxy.code.CacheProxy;
 import study.kyh.principle3.pure_proxy.proxy.code.ProxyPatternClient;
 import study.kyh.principle3.pure_proxy.proxy.code.RealSubject;
+import study.kyh.principle3.pure_proxy.proxy.code.Subject;
 
 public class ProxyPatternTest {
     @Test
     void noProxyTest() {
         RealSubject realSubject = new RealSubject();
         ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @Test
+    void cacheProxyTest() {
+        Subject realSubject = new RealSubject();
+        Subject cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
         client.execute();
         client.execute();
         client.execute();
