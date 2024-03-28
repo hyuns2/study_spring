d40f4f860db62cab3cfbad1a2aba599cdb49f509
hyuns2 <hyunsoi0103@gmail.com>
2024-03-28 12:43:34 +0900

    Chore: update Principle3Application

--- src/main/java/study/kyh/principle3/Principle3Application.java
+++ src/main/java/study/kyh/principle3/Principle3Application.java
@@ -8,12 +8,14 @@ import study.kyh.principle3.config.AppV1Config;
 import study.kyh.principle3.config.AppV2Config;
 import study.kyh.principle3.config.v1_proxy.ConcreteProxyConfig;
 import study.kyh.principle3.config.v1_proxy.InterfaceProxyConfig;
+import study.kyh.principle3.config.v2_dynamic_proxy.handler.DynamicProxyBasicConfig;
 import study.kyh.principle3.trace.log_trace.LogTrace;
 import study.kyh.principle3.trace.log_trace.ThreadLocalLogTrace;
 
 // @Import({AppV1Config.class, AppV2Config.class})
 // @Import(InterfaceProxyConfig.class)
-@Import(ConcreteProxyConfig.class)
+//@Import(ConcreteProxyConfig.class)
+@Import(DynamicProxyBasicConfig.class)
 @SpringBootApplication(scanBasePackages = "study.kyh.principle3.app.v3")
 public class Principle3Application {
 
